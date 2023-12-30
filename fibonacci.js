e87696253e879557ab0c9aa04b0486255e5d2b74
JBisquick <felipebirch@gmail.com>
2023-12-29 19:40:34 -0800

    Add function making fibonacci array with recursion

--- fibonacci.js
+++ fibonacci.js
@@ -13,3 +13,18 @@ function fibs(n) {
   }
   
   console.log(fibs(10));
+
+function fibsRec(n) {
+  if (n < 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    const next = fibsRec(n - 1)[n-2] + fibsRec(n - 1)[n-3];
+    return fibsRec(n - 1).concat([next]);
+  }
+}
+
+console.log(fibsRec(10));  
